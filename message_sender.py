30fae09da189d879b5ef0d9af1755f3759fb666b
Gabriel Mukobi <gabrielmukobi@gmail.com>
2018-03-12 21:42:38 -0700

    Round 2

--- message_sender.py
+++ message_sender.py
@@ -7,13 +7,14 @@ class Assassin:
     def __init__(self, content):
         self.name = content[0]
         self.my_target = content[1]
-        self.status = content[2]
-        self.round_of_status = content[3]
-        self.grade = content[4]
-        self.section = content[5]
-        self.number = content[6]
-        self.has_new_target = content[7]
-        self.error = content[8]
+        self.targeted_by = content[2]
+        self.status = content[3]
+        self.round_of_status = content[4]
+        self.grade = content[5]
+        self.section = content[6]
+        self.number = content[7]
+        self.has_new_target = content[8]
+        self.error = content[9]
 
     def set_my_target(self, target):
         self.my_target = target
@@ -26,18 +27,18 @@ def send_to_all(all_assassins):
     if input("Are you sure you want to send a message to everyone?") != "yes":
         print("Exiting")
         exit(1)
-    for assassin in all_assassins[74:]:
-        if False:
+    for assassin in all_assassins:
+        if "Yes" not in assassin.has_new_target or "Alive" not in assassin.status:
             continue
         section = assassin.section
         if section == '':
             section = "Oops, you never wrote down a section!"
-        message = (assassin.name + ", be prepared! Your target is " + assassin.my_target
-                   + ". Confirm by replying 'subito'")
+        message = (assassin.name + ", your new target target is " + assassin.my_target
+                   + ". Confirm you got this message by replying 'sforzando'")
 
         encoded_message = requests.utils.quote(message, safe='')
 
-        gateway = "http://192.168.1.154:8766/"
+        gateway = "http://172.31.18.248:8766/"
 
         number = assassin.number
 
